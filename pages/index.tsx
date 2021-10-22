803bbca4f507acbb8c8520ca3f8b148a8fd06b7d
Kekelberi <alexei.gaponov@gmail.com>
2021-10-22 18:32:31 +0300

    probel

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ const Home: NextPage = () => {
     <div>
       <UserList />
       you are the best
+      
 
     </div>
   )
--- src/action/user.tsx
+++ src/action/user.tsx
@@ -2,34 +2,7 @@ import axios from "axios"
 import { Dispatch } from "redux"
 import { UserActions, UserActionType } from "../types/usersInterface"
 
-//export const fetchusers = () => {
-//	return async (dispatch: Dispatch<UserActions>) => {
-//		try {
-//			dispatch({ type: UserActionType.FETCH_USERS })
-//			const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-//			setTimeout(() => {
-//				dispatch({ type: UserActionType.FETCH_USERS_SUCCES, payload: response.data })
-//			}, 2000);
-//		} catch (e) {
-//			dispatch({ type: UserActionType.FETCH_USERS_ERROR, payload: "моя ошибка" })
 
-//		}
-//	}
-//}
-
-//export const fetchusers = () => {
-//	return async (dispatch: Dispatch<UserActions>) => {
-//		try {
-//			dispatch({ type: UserActionType.FETCH_USERS, })
-//			const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-//			setTimeout(() => {
-//				dispatch({type: UserActionType.FETCH_USERS_SUCCES, payload: response.data})
-//			}, 5000);
-//		} catch (e) {
-//			dispatch({ type: UserActionType.FETCH_USERS_ERROR, payload: 'моя ошибка' })
-//		}
-//	}
-//}
 
 export const fetchusers =() => {
 	return async(dispatch: Dispatch<UserActions>) => {
